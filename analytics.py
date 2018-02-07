5946130ba027a50a893c6bf35cc857edc90804ef
ateamsf <ibhargav92@gmail.com>
2018-02-06 21:49:12 -0800

    Email validation module

--- analytics.py
+++ analytics.py
@@ -1,21 +1,22 @@
 import pymysql
 import pymysql.cursors
 import json
+import re 
 
 
-# -------- Connection to the Database ---------
+# -------- Connection to the Database ------------------------------------------------
 
 connection = pymysql.connect(host='127.0.0.1',
 			     user='root',
-			     password='password',
-			     db = 'banking',
+			     password='pwd',   #Enter your sql db password here
+			     db = 'banking',   #Enter your db name here
 			     charset='utf8mb4')
 
 
 # -------- Assuming this is the JSON record received from Bank's API Endpoint ---------
 
 json_data = {
-		"account_id" : 1454581, 
+		"account_id" : 1454582, 
 		"event_date": "2018-01-09", 
 		"account_standing": "B", 
 		"account_information": {
@@ -29,48 +30,48 @@ json_data = {
 		"city": "Mount Pleasant", 
 		"state": "CA", 
 		"zip_code": "90010"}, 
-		"email_address": "hello@yahoo.com"} 
+		"email_address": "jsmith@yahoo.com"} 
 	   }
 
 
-# -------- Updating database if changes found in JSON record ---------
+# -------- Updating database if changes found in JSON record ----------------------------
 
 def update_email():
 
 	# Account ID from json record
-	account_check = json_data["account_id"]
+	account_id_check = json_data["account_id"]
 
 	# Email ID from json record
-	email_check = json_data["account_information"]["email_address"]
+	email_id_update = json_data["account_information"]["email_address"]
 
 	try:
 		with connection.cursor() as cursor:			
-			# Read account ID from database
-			cursor.execute("SELECT account_id FROM clients")
-			row = cursor.fetchone()
+			
+			# Check for user requested account ID from database 
+			sql = "SELECT account_id FROM clients WHERE account_id = %s"
+			cursor.execute(sql, account_id_check)
+			
+			rows = cursor.fetchone()
 
-			for i in row:							
+			# Executes when account Id is found in DB.
+			if rows != None:
+				# Email address validation
+				if re.match("(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email_id_update) != None:
 				
-				# Validating account ID with existing database
-				if row[0] == account_check:
-					
 					# Update email address in database
 					sql = "UPDATE clients SET email_address = %s WHERE account_id = %s"
-					cursor.execute(sql, (email_check, account_check))
+					cursor.execute(sql, (email_id_update, account_id_check))
 
 					# Necessary to commit since it's not auto update. 
 					connection.commit()
 
-				else:
-					# For similar email update request from the user
-					return "It's already up-to-date"
+					print("Email address has been updated to:", email_id_update)
 
-		with connection.cursor() as cursor:
-			# Read updated email address from database
-			cursor.execute("SELECT email_address FROM clients")
-			row = cursor.fetchone()
-			
-			print("Email address updated successfully to:", row[0])		
+				else:
+					print("Email address entered is Invalid")
+
+			else:
+				print("Sorry! Your account doesn't exist.")
 
 	finally:
 		cursor.close()
